910dd101589eddef8b0a76e95152e35faccfc92c
itschuza11-wq <itschuza11@gmail.com>
2025-11-16 15:30:04 +0500

    Delete admin directory

--- admin/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Admin() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
-    </div>
-  );
-}
